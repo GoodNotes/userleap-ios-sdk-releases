dd9fab059512491917095af68b92e612dc472f5f
0xpablo <pablos93@gmail.com>
2023-02-27 09:44:00 +0100

    Add dummy Mac Catalyst platform to workaround a SPM bug
    This workaround shouldn’t be needed anymore in Xcode 14.3 (Swift 5.8) and later.

---
+++ UserLeapKit.xcframework/ios-arm64_x86_64-maccatalyst/UserLeapKit.framework/Versions/A/Headers/UserLeapKit.h
@@ -0,0 +1,17 @@
+//
+//  UserLeapKit.h
+//  UserLeapKit
+//
+//  Created by Pablo Carcelén on 27/2/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UserLeapKit.
+FOUNDATION_EXPORT double UserLeapKitVersionNumber;
+
+//! Project version string for UserLeapKit.
+FOUNDATION_EXPORT const unsigned char UserLeapKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserLeapKit/PublicHeader.h>
+
